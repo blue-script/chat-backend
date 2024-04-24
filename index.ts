a56fc9b7fee6a8ae3a504b1f90d052bb236b6c48
blue-script <alexm92@inbox.ru>
2024-04-24 22:54:14 +0500

    fix vercel.json

--- index.ts
+++ index.ts
@@ -1,23 +1,23 @@
 import express from 'express';
 import { createServer } from 'node:http';
-import { fileURLToPath } from 'node:url';
-import { dirname, join } from 'node:path';
-import { Server } from 'socket.io';
+// import { fileURLToPath } from 'node:url';
+// import { dirname, join } from 'node:path';
+// import { Server } from 'socket.io';
 
 const index = express();
 const server = createServer(index);
-const io = new Server(server);
+// const io = new Server(server);
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+// const __dirname = dirname(fileURLToPath(import.meta.url));
 
 index.get('/', (req, res) => {
   // res.sendFile(join(__dirname, 'index.html'));
   res.send('Hello, it`s me ')
 });
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-});
+// io.on('connection', (socket) => {
+//   console.log('a user connected');
+// });
 
 server.listen(3009, () => {
   console.log('server running at http://localhost:3009');
