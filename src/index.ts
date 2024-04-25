7b02de4977b63da39a0d87689b29c31d4dfce884
blue-script <alexm92@inbox.ru>
2024-04-25 17:41:50 +0500

    fix: vercel

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,7 @@ const server = createServer(index)
 const io = new Server(server, {
   cors: {
     origin: 'http://localhost:3000',
-    methods: ['GET', 'POST'],
-    credentials: true
+    methods: ['GET', 'POST']
   }
 })
 
@@ -21,12 +20,31 @@ const messages = [
   {id: '2123', message: 'hello, Aleks', user: {id: 'asdfsdf', name: 'Serg'}}
 ]
 
+const usersState = new Map()
+
 io.on('connection', (socket) => {
-  console.log('a user connected')
+  usersState.set(socket, {id: new Date().getTime().toString(), name: "anonym"} )
+
+  socket.on('disconnect', ()=>{
+    usersState.delete(socket)
+  })
+
+
+  socket.on('client-name-sent',( name: string)=>{
+    if (typeof name !== 'string') return
+
+    const user = usersState.get(socket)
+    user.name = name
+  })
 
   socket.on('client-message-sent', (message: string) => {
-    const messageItem = {id: '2342' + new Date().getTime(), message: message, user: {id: 'asdfdfsd', name: 'Aleks'}}
+    if (typeof message !== 'string') return
+
+    const user = usersState.get(socket)
+
+    const messageItem = {id: new Date().getTime().toString(), message: message, user: {id: user.id, name: user.name}}
     messages.push(messageItem)
+
     socket.emit('new-message-sent', messageItem)
   })
 
